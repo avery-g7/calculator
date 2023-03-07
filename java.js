605cdbf90af5928a7f8eb7e0a4499d10869e8add
Avery <averygadd7@gmail.com>
2023-03-06 19:00:28 -0700

    functions to get numbers and operators

--- java.js
+++ java.js
@@ -2,18 +2,19 @@ let currentOperation = null;
 let operand = '';
 let secondOperand = '';
 
-
 const clearBtn = document.getElementById('clear');
 const deleteBtn = document.getElementById('delete');
 const equalsBtn = document.getElementById('equals');
 const screen = document.getElementById('screen');
 const decimal = document.getElementById('decimal');
-const numberBtns = document.querySelectorAll(['number']);
+const numberBtns = document.querySelectorAll('.number');
+const opButtons = document.querySelectorAll('.op');
 
 clearBtn.addEventListener('click', refreshCalc);
 deleteBtn.addEventListener('click', del);
 equalsBtn.addEventListener('click', solve);
-
+window.addEventListener('keydown', keyInput);
+decimal.addEventListener('click', getDecimal);
 
 //Button functions
 
@@ -30,20 +31,28 @@ function del(){
     .slice(0, -1);
 }
 
-window.addEventListener('keydown', function (e) {
-  screen.textContent = e.key;
-});
 numberBtns.forEach(button => {
-  button.addEventListener('click', () => button.textContent)
+  button.addEventListener('click', () => getNumber(button.textContent));
 })
-//numberBtns.addEventListener('click', (e) => {
-//    let screen = e.target.value;
-//});
 
-// numberBtns.addEventListener('click', function(button) {
-//   screen.textContent = button.value;
-// });
+function getNumber(num) {
+  if(screen.textContent === '0') {
+    refresh();
+  };
+  screen.textContent += num;
+};
 
+opButtons.forEach(button => {
+  button.addEventListener('click', () => getOperator(button.textContent));
+})
+
+function getOperator(op) {
+  if (currentOperation !== null) {
+    solve();
+  };
+  operand = screen.textContent;
+  currentOperation = op;
+};
 
 function solve() {
   if (currentOperation === '÷' && screen.textContent === '0') {
@@ -55,9 +64,6 @@ function solve() {
     operate(currentOperation, operand, secondOperand));
 };
 
-function roundToTenth(num) {
-  return Math.round(num * 100) / 100
-}
 
 //key functions when pressed
 
@@ -66,6 +72,29 @@ function keyOperator(key) {
   if(key === 'x' || '*') return 'x';
   if(key === '-') return '-';
   if(key === '+') return '+';
+}
+
+function keyInput(e) {
+  if(e.key >= 0 && e.key <= 9) {
+    getNumber(e.key)
+  } else if (e.key === '-' || '+' || '/' || '*') {
+    getOperator(keyOperator(e.key));
+  } else if (e.key === '=' || e.key === 'Enter') {
+    solve();
+  } else if (e.key === 'Backspace') {
+    del();
+  } else if (e.key === '.') {
+    getDecimal();
+  };
+};
+
+function getDecimal() {
+  if (screen.textContent === '') {
+    screen.textContent = '0';
+  }else if (screen.textContent.includes('.')){
+    return
+  };
+  screen.textContent += '.';
 }
 
 // Operator functions
@@ -78,7 +107,6 @@ function subtract(a, b) {
 	return a - b;
 };
 
-
 function multiply(a, b) {
   return a * b;
 };
@@ -86,6 +114,7 @@ function multiply(a, b) {
 function division(a, b) {
   return a / b;
 };
+
 function operate(operator, a, b) {
   a = Number(a);
   b = Number(b);
@@ -106,3 +135,13 @@ function operate(operator, a, b) {
       return null;
   };
 };
+
+//random functions
+
+function refresh() {
+  screen.textContent = '';
+}
+
+function roundToTenth(num) {
+  return Math.round(num * 100) / 100
+}
